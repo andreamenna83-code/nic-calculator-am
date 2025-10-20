2e0dd529f97443809254f2214a5ad897ec8c11d2
andreamenna83-code <andreamenna83@gmail.com>
2025-10-21 00:52:26 +0200

    Update Nic Calculator AM to v6.2

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,30 @@
+const CACHE = 'nic-am-v62';
+const ASSETS = [
+  '/nic-calculator-am/',
+  '/nic-calculator-am/index.html',
+  '/nic-calculator-am/styles.css',
+  '/nic-calculator-am/manifest.webmanifest',
+  '/nic-calculator-am/icons/icon-192.png',
+  '/nic-calculator-am/icons/icon-256.png',
+  '/nic-calculator-am/icons/icon-384.png',
+  '/nic-calculator-am/icons/icon-512.png'
+];
+
 self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open('nic-am-v1').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './app.js',
-      './styles.css',
-      './manifest.webmanifest',
-      './icons/icon-192.png',
-      './icons/icon-512.png'
-    ]))
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
+  self.clients.claim();
 });
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+  const req = event.request;
+  if (req.mode === 'navigate') {
+    event.respondWith(fetch(req).catch(() => caches.match('/nic-calculator-am/index.html')));
+    return;
+  }
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req)));
 });
